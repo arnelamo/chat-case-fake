5d4ab7401961be9db95e120e09e4683f9dc51dbd
Arne Pedersen <arne.lamo@gmail.com>
2023-06-15 15:55:32 +0200

    styling added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -40,25 +40,69 @@
 
 		input = '';
 	};
+
+	const getNameTag = (name: string | undefined) => {
+		if (!name) return '?';
+		return name.charAt(0).toLocaleUpperCase();
+	};
 </script>
 
 <main>
 	<section>
-		<h2>Chat</h2>
-		<div>
-			{#each chatLog as { senderId, message }, index}
-				<article aria-posinset={index + 1} aria-setsize={chatLog.length}>
-					<h2>{senderId}</h2>
-					<p>{message}</p>
-				</article>
-			{/each}
+		<h2 class="text-3xl font-semibold">Superchat</h2>
+		<div class="bg-gray-50 rounded-2xl mt-8 p-8">
+			<div class="flex-col space-y-4">
+				{#each chatLog as { name, message, senderId }, index}
+					{@const myUser = senderId === 1}
+					<article
+						aria-posinset={index + 1}
+						aria-setsize={chatLog.length}
+						class={`flex items-center ${myUser ? 'justify-end' : 'justify-start'}`}
+					>
+						<span
+							class={`flex items-center justify-center h-10 w-10 rounded-full bg-indigo-200 flex-shrink-0 `}
+							>{getNameTag(name)}</span
+						>
+						<p
+							class={`${
+								myUser ? 'order-first mr-2' : 'order-last ml-2'
+							} bg-white p-4 rounded-md shadow-sm`}
+						>
+							{message}
+						</p>
+					</article>
+				{/each}
+			</div>
+			<form on:submit={handleSubmit} class="flex w-full items-center space-x-2 pt-4">
+				<label class="w-full">
+					<input
+						type="text"
+						bind:value={input}
+						required
+						class="w-full text-lg px-2 border border-gray-500 rounded-md"
+					/>
+				</label>
+				<button
+					type="submit"
+					class="flex items-center bg-indigo-500 text-white py-1 px-4 rounded-md hover:opacity-80"
+				>
+					<span class="pr-2">Send</span>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-4 h-4"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+						/>
+					</svg>
+				</button>
+			</form>
 		</div>
 	</section>
-	<form on:submit={handleSubmit}>
-		<label>
-			New Message:
-			<input type="text" bind:value={input} required />
-		</label>
-		<button type="submit">Submit</button>
-	</form>
 </main>
